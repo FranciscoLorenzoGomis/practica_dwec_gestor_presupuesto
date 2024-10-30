09ca67d9e2fe29a1b593d3d8d01927b9b88eac9f
Francisco Lorenzo Gomis <fralorgom@alu.edu.gva.es>
2024-10-30 09:58:30 +0100

    Practice Fundamentos de JavaScript I

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,25 +1,61 @@
+"use strict"
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
 
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valorIntroducido) {
+
+    let numero;
+    if (valorIntroducido >= 0 && typeof valorIntroducido === 'number') {
+        presupuesto = valorIntroducido;
+        numero = presupuesto;
+    }
+    else {
+        numero = -1;
+    }
+
+    return numero;
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`
 }
 
-function CrearGasto() {
+function CrearGasto(descripcion, valor) {
     // TODO
+    this.descripcion = descripcion;
+    if(valor >= 0 && typeof valor === 'number' ){
+        this.valor = valor;
+    }
+    else{
+        this.valor = 0;
+    }
+
+    this.mostrarGasto = function(){
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+       }
+
+    this.actualizarDescripcion = function (descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    this.actualizarValor = function (valor) {
+        if(valor >= 0 && typeof valor === 'number')
+        {
+          this.valor = valor;
+        }
+    }
+
 }
 
-// NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
-// Las funciones y objetos deben tener los nombres que se indican en el enunciado
-// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto
-}
+    // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
+    // Las funciones y objetos deben tener los nombres que se indican en el enunciado
+    // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
+    export {
+        mostrarPresupuesto,
+        actualizarPresupuesto,
+        CrearGasto
+    }
